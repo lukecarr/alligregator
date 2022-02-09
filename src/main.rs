1107fd35d1d2b1ef09abd643c9815f38ee6fbca4
Luke Carr <luke_jcarr@icloud.com>
2022-02-09 12:16:57 +0000

    ✨ Added missing file handling

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,19 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let out = File::create(args.output)?;
+    let out = File::create(args.output.clone())?;
     let mut out = BufWriter::new(out);
 
     for (i, folder) in args.folders.split(',').enumerate() {
         let path = Path::new(&args.root).join(folder).join(&args.filename);
+
+        if !path.exists() {
+            println!(
+                "'{}' not found in folder '{}', so skipping...",
+                args.filename, folder
+            );
+            continue;
+        }
         let file = File::open(path)?;
         let mut reader = BufReader::new(file);
 
@@ -42,6 +50,7 @@ fn main() -> io::Result<()> {
         for line in reader.lines().filter_map(|result| result.ok()) {
             writeln!(out, "{},{}", folder, line)?;
         }
+        println!("{} written to {} successfully!", folder, args.output);
     }
 
     Ok(())
